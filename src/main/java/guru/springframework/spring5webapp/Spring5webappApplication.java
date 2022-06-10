0b99d99f34dcd807c768561fbc0e349dd178d764
Jean-Rene Lingofe <rene.lingofe@proxytechnology.net>
2022-06-10 12:50:21 +0100

    Added a Integer

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5webappApplication {
+	public Integer number;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
