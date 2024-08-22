18b87f63194767e0caaffae47e9acd42e355e906
K1 <k1fullstackk@gmail.com>
2024-08-22 14:24:49 +0300

    attribute and controller

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,4 +1,20 @@
 package guru.springframework.spring5webapp.controllers;
 
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
 public class BookController {
+    private final BookRepository bookRepository;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());// this attribute put the value of the books in the html file!
+
+        return "books/list" ;
+    }
 }
