b90135927c4aa210e99b6daab9385997203c7d61
Ghislain Muabila <ghislainmuabila@ghislains-mbp.home>
2024-09-18 02:39:10 +0100

    Added Rest Endpoint for Jenkins Demo

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,10 +2,17 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Spring5webappApplication {
-	public Integer number;
+
+	@GetMapping
+	public String message(){
+		return "Welcome to GTech first Jenkins project...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
